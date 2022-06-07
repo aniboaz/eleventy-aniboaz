50f3b38f6f147afc2b5f9873f11ae9fed959afc2
aniboaz <aniboaz@gmail.com>
2022-06-07 13:51:58 +0300

    maybe syntax highlight

--- .eleventy.js
+++ .eleventy.js
@@ -9,6 +9,13 @@ module.exports = function(eleventyConfig) {
   // Eleventy Navigation https://www.11ty.dev/docs/plugins/navigation/
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
 
+//syntax
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function(eleventyConfig) {
+  eleventyConfig.addPlugin(syntaxHighlight);
+};
+
   // Configuration API: use eleventyConfig.addLayoutAlias(from, to) to add
   // layout aliases! Say you have a bunch of existing content using
   // layout: post. If you don’t want to rewrite all of those values, just map
